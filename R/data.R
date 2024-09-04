906c34ceceee715ded8a65d67d11184eeeb14bc0
Lucia Lorenzi <lucialorenzi90@gmail.com>
2024-09-04 11:04:30 +0200

    added minimal data documentation

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Overlapping classcodes
+#'
+#' classcodes used to classify assembled transfrags present in the tracking
+#' files from the GffCompare utility were classified in overlapping
+#' a reference transcript (in same strand) and non-overlapping
+#'
+#' @format A character vector of length 9 : "=" "c" "e" "o" "j" "k" "m" "n" "p"
+"overlapping_class_codes"
+
+#' Non-overlapping classcodes
+#'
+#' classcodes used to classify assembled transfrags present in the tracking
+#' files from the GffCompare utility were classified in overlapping
+#' a reference transcript (in same strand) and non-overlapping
+#'
+#' @format A character vector of length 7: "i" "s" "r" "u" "x" "y" "."
+"non_overlapping_class_codes"
